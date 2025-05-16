96b40b6a4a34c8a1cea093bb68e8cca94bacc1bd
Ars Paradox <152275094+Iteranya@users.noreply.github.com>
2025-05-16 16:16:02 +0700

    Fixed Critical Bug

--- static/aina/scripts/previewManager.js
+++ static/aina/scripts/previewManager.js
@@ -1,13 +1,17 @@
 import { getProject } from "./dbIntegration.js";
 
 export async function initPreview(htmlCode, preview, slug) {
+
     // Set up event listener for editor changes
     htmlCode.addEventListener('input', () => updatePreview(htmlCode, preview));
     // Initial update
-    const project = await getProject(slug);  // Added 'const' declaration here
-    if (project.html) {
-        htmlCode.value = project.html;
+    if(slug){
+        const project = await getProject(slug);  // Added 'const' declaration here
+            if (project.html) {
+                htmlCode.value = project.html;
+            }
     }
+   
     
 
     updatePreview(htmlCode, preview);
