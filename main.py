57ccb3af741b3ccdff312476f0232218cdc73a02
Ars Paradox <152275094+Iteranya@users.noreply.github.com>
2025-05-19 19:22:28 +0700

    Change localhost to zero
    For...
    
    I dunno...

--- main.py
+++ main.py
@@ -46,4 +46,4 @@ app.include_router(
 # Run the application with: uvicorn main:app --reload
 if __name__ == "__main__":
     
-    uvicorn.run("main:app", host="localhost", port=5451, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=5451, reload=True)
