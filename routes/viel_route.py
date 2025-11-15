a6f0c93a61a626446149852e5272af04e057e296
Ars Paradox <152275094+Iteranya@users.noreply.github.com>
2025-11-15 17:13:40 +0700

    Get rid of Viel lmao

--- routes/viel_route.py
+++
@@ -1,6 +0,0 @@
-from fastapi import APIRouter, Depends, UploadFile, File, HTTPException
-from fastapi.responses import FileResponse
-import os
-import shutil
-
-router = APIRouter(prefix="/ai", tags=["AI"])
